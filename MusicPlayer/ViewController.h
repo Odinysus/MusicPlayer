10e1f8a75ef28d27878c92b23a2c2de431c7248f
johnAppGit <caidongqiang@hotmail.com>
2015-10-30 10:03:16 +0800

    Initial Commit

---
+++ MusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicPlayer
+//
+//  Created by 强 on 15/10/30.
+//  Copyright (c) 2015年 强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
